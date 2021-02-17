e0d70cce29d87393c43d48540d63e5320ce543ff
Alexandre Antonio Juca <corextechnologies@gmail.com>
2021-02-17 15:31:07 +0100

    implement constructor and initialize fields

--- payment_gateway.php
+++ payment_gateway.php
@@ -5,5 +5,25 @@
  */
 class WP_Vpos_Gateway extends WC_Payment_Gateway
 {
-    
+    private $token;
+    private $pos_id;
+
+    public function __construct()
+    {
+        $this->id = "vPOS woocommerce";
+        $this->method_title = $this->get_option('title');
+        $this->title = $this->get_option('title');
+        $this->method_description = "The one stop shop for online payments in Angola, allowing you to process payments requests from EMIS GPO through vPOS.";
+        $this->icon = "https://developer.vpos.ao/images/logo-366e50cc.png";
+        $this->init_form_fields();
+        $this->init_settings();
+        $this->token = $this->get_option('vpos-token');
+        $this->pos_id = $this->get_option('pos_id');
+        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+    }
+
+    public function init_form_fields()
+    {
+        $this->form_fields = include("vpos-settings.php");
+    }
 }
