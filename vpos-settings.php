48010d03807202c2728afc5b2cb753cf01ed5fa4
alexjuca <alexandre.juca@nextbss.co.ao>
2021-02-19 14:26:22 +0100

    improve settings

--- vpos-settings.php
+++ vpos-settings.php
@@ -8,7 +8,7 @@ return array(
     'enabled' => array(
         'title' => __('Enable/Disable', 'woocommerce'),
         'type' => 'checkbox',
-        'label' => __('Activar vPOS Checkout', 'vPOS'),
+        'label' => __('Activate vPOS', 'vPOS'),
         'default' => 'yes'
     ),
     'title' => array(
@@ -28,17 +28,17 @@ return array(
     ),
     'vpos_token' => array(
         'title' => __('vPOS Token', 'vPOS'),
-        'type' => 'title',
+        'type' => 'text',
         'description' => 'O token de API fornecido por vPOS'
     ),
     'gpo_pos_id' => array(
-        'title' => __('POS ID*', 'vPOS'),
+        'title' => __('POS Id*', 'vPOS'),
         'type' => 'text',
         'description' => __('O ID do ponto de venda fornecido pela EMIS', 'vPOS'),
         'desc_tip' => true
     ),
     'gpo_supervisor_card' => array(
-        'title' => __('Cartão do supervisor*', 'vPOS'),
+        'title' => __('Supervisor Card*', 'vPOS'),
         'type' => 'text',
         'description' => __('Cartão do supervisor fornecido pela EMIS', 'woocommerce'),
         'desc_tip' => true
@@ -56,16 +56,16 @@ return array(
         'desc_tip' => true
     ),
     'vpos_environment' => array(
-        'title'       => __('Modo Sandbox', 'vPOS'),
+        'title'       => __('Sandbox mode', 'vPOS'),
         'type'        => 'checkbox',
         'label'       => __('Habilitar modo Sandbox', 'vPOS'),
         'default'     => 'no',
         'description' => "O ambiente que será usado e.g SANDBOX ou PRODUCTION",
     ),
     'merchant' => array(
-        'title'       => __('Nome da sua loja ou comerciante', 'vPOS' ),
+        'title'       => __('Your store name', 'vPOS' ),
         'type'        => 'text',
-        'label'       => __('Nome do comerciante', 'vPOS' ),
+        'label'       => __('Your store or company name', 'vPOS' ),
         'description' => "O nome do comerciante, loja ou estabelecimento",
     )		
 );
