2853b2b7158f801ef520a5a1512887098fc638ea
alexjuca <alexandre.juca@nextbss.co.ao>
2021-03-16 13:07:28 +0100

    Add missing php init block anf format code correctly

--- src/request_handler.php
+++ src/request_handler.php
@@ -1,49 +1,51 @@
+<?php
+
 class RequestHandler {
 
-public function __construct() {
-
-}
-
-public function handlePayment($vpos, $mobile, $amount) {
-    
-    $response_data = $vpos->newPayment($mobile, $amount);
-
-    if ($response_data["message"] == "Accepted") {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo $response_data["location"];
-    } else {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo $response_data["message"];
+    public function __construct() { 
+        // Empty Constructor
     }
-}
 
-public function handlePollResource($vpos, $id) {
-    $response_data = $vpos->pollResource($id);
+    public function handlePayment($vpos, $mobile, $amount) {
+        
+        $response_data = $vpos->newPayment($mobile, $amount);
 
-    if ($response_data["message"] == "See Other") {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo $response_data["location"];
-    } else {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo $response_data["message"];
+        if ($response_data["message"] == "Accepted") {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo $response_data["location"];
+        } else {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo $response_data["message"];
+        }
     }
-}
 
-public function handleGetTransaction($vpos, $id) {
-    $response_data = $vpos->getTransaction($id);
+    public function handlePollResource($vpos, $id) {
+        $response_data = $vpos->pollResource($id);
 
-    if ($response_data["code"] == 200) {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo json_encode($response_data["decoded_body"]);
-    } else {
-        header('Content-Type: application/json');
-        http_response_code($response_data["code"]);
-        echo $response_data["body"];
+        if ($response_data["message"] == "See Other") {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo $response_data["location"];
+        } else {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo $response_data["message"];
+        }
     }
-}
+
+    public function handleGetTransaction($vpos, $id) {
+        $response_data = $vpos->getTransaction($id);
+
+        if ($response_data["code"] == 200) {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo json_encode($response_data["decoded_body"]);
+        } else {
+            header('Content-Type: application/json');
+            http_response_code($response_data["code"]);
+            echo $response_data["body"];
+        }
+    }
 }
