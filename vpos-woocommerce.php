955e5b5ab8d456cefee8c924ab5b29ac89764c4f
Alexandre Antonio Juca <corextechnologies@gmail.com>
2021-03-18 12:28:50 +0100

    update license to MIT

--- vpos-woocommerce.php
+++ vpos-woocommerce.php
@@ -9,8 +9,7 @@
  * Requires PHP:      7.2
  * Author:            Next Business Solution
  * Author URI:        https://github.com/nextbss
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License:           MIT License
  * Text Domain:       vpos-woocommerce-plugin
  * Domain Path:       /languages
  */
