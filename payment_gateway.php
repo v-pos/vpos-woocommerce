e775d09882f5c7877f6362113b7675fab82dcff7
alexjuca <alexandre.juca@nextbss.co.ao>
2021-03-24 09:15:00 +0100

    prevent unauthorized access to code from outside Wordpress

--- payment_gateway.php
+++ payment_gateway.php
@@ -14,6 +14,10 @@
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 class WP_Vpos_Gateway extends WC_Payment_Gateway
 {
     private $token;
--- src/vpos_order_handler.php
+++ src/vpos_order_handler.php
@@ -14,6 +14,10 @@
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 class VposOrderHandler {
     
     public static function completeOrder($order_id) {
