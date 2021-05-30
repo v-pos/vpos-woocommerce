a03f259a5c7f80533c946b6aec48c0dd6ef5aa65
alexjuca <alexandre.juca@nextbss.co.ao>
2021-05-30 14:46:25 +0100

    Add class responsable for vpos endpoints

---
+++ src/vpos_endpoint.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("uuid.php");
+
+class VPOS_Routes extends WP_REST_Controller {
+
+    public function __construct() {
+        
+    }
+    
+    public function register_routes() {
+        error_log("invoked register_routes!!");
+        $namespace = "vpos-woocommerce/v1";
+        $base = "payments";
+
+        // Endpoint -> http://your-site.com/wp-json/vpos-woocommerce/v1/payments/fc4d77b0-a4c2-4417-b537-a62f7c88dd06
+        register_rest_route($namespace, $base . "/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/", array(
+            array(
+                "methods"       => "GET",
+                "callback"      => array($this, 'perform_payment')
+            )
+        ));
+    }
+
+
+ /**
+ * Perform payment
+ *
+ * @param WP_REST_Request $request Full data about the request.
+ * @return WP_Error|WP_REST_Response
+ */
+  public function perform_payment($request) {    
+    return new WP_REST_Response(uuid(), 200);
+  }
+}
