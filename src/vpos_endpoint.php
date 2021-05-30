95f5455b12a147e868ab2e3cf12608fbdeb447f7
alexjuca <alexandre.juca@nextbss.co.ao>
2021-05-30 17:02:21 +0100

    update endpoint

--- src/vpos_endpoint.php
+++ src/vpos_endpoint.php
@@ -11,9 +11,9 @@ class VPOS_Routes extends WP_REST_Controller {
     public function register_routes() {
         error_log("invoked register_routes!!");
         $namespace = "vpos-woocommerce/v1";
-        $base = "payments";
+        $base = "cart/vpos";
 
-        // Endpoint -> http://your-site.com/wp-json/vpos-woocommerce/v1/payments/fc4d77b0-a4c2-4417-b537-a62f7c88dd06
+        // Endpoint -> http://your-site.com/wp-json/vpos-woocommerce/v1/cart/vpos/fc4d77b0-a4c2-4417-b537-a62f7c88dd06
         register_rest_route($namespace, $base . "/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/", array(
             array(
                 "methods"       => "GET",
