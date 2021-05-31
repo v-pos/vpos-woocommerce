319960701593d5cea4be275a6db3b3dd384a3bbb
alexjuca <alexandre.juca@nextbss.co.ao>
2021-05-31 16:49:15 +0100

    move code to create table to repository

--- src/db/transaction_repository.php
+++ src/db/transaction_repository.php
@@ -14,6 +14,29 @@ class TransactionRepository {
           "SELECT * FROM $this->transactions_table WHERE ID = '$uuid'"
         );
     }
+
+    // TODO: This should probably be moved to a different class since it is not the 
+    // repositories responsability to create the table schema.
+    public function create_transactions_table() {
+        $charset_collate = $this->db->get_charset_collate();
+
+        $sql = "CREATE TABLE $this->transactions_table (
+            id varchar(255) PRIMARY KEY NOT NULL,
+            transaction_id varchar(255),
+            status varchar(255),
+            type varchar(255),
+            amount varchar(255),
+            mobile varchar(255),
+            status_reason varchar(255),
+            created_at timestamp,
+            updated_at timestamp,
+
+            INDEX (id)
+          ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
     
     public function update_transaction($id, $body) {
         $type = $body->{"type"};
