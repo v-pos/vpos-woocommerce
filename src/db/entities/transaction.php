8d57e7c7b3afbd6ce8530062a30fd3d66b34ebf1
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-07 12:55:30 +0100

    format code

--- src/db/entities/transaction.php
+++ src/db/entities/transaction.php
@@ -1,6 +1,7 @@
 <?php
 
-class Transaction {
+class Transaction
+{
     private $uuid;
     private $transaction_id;
     private $amount;
@@ -10,7 +11,8 @@ class Transaction {
     private $type;
     private $order_id;
 
-    public function __construct($uuid, $transaction_id, $amount, $mobile, $status, $status_reason, $type, $order_id) {
+    public function __construct($uuid, $transaction_id, $amount, $mobile, $status, $status_reason, $type, $order_id)
+    {
         $this->uuid = trim($uuid);
         $this->transaction_id = trim($transaction_id);
         $this->amount = trim($amount);
@@ -21,35 +23,43 @@ class Transaction {
         $this->order_id = trim($order_id);
     }
 
-    public function get_uuid() {
+    public function get_uuid()
+    {
         return $this->uuid;
     }
 
-    public function get_transaction_id() {
+    public function get_transaction_id()
+    {
         return $this->transaction_id;
     }
 
-    public function get_amount() {
+    public function get_amount()
+    {
         return $this->amount;
     }
 
-    public function get_mobile() {
+    public function get_mobile()
+    {
         return $this->mobile;
     }
 
-    public function get_status() {
+    public function get_status()
+    {
         return $this->status;
     }
 
-    public function get_status_reason() {
+    public function get_status_reason()
+    {
         return $this->status_reason;
     }
 
-    public function get_type() {
+    public function get_type()
+    {
         return $this->type;
     }
 
-    public function get_order_id() {
+    public function get_order_id()
+    {
         return $this->order_id;
     }
 }
