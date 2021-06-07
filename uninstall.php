642d65ea804f272b019ade04249469d21dec188e
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-07 12:56:54 +0100

    format code

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,15 @@
 <?php
 
 // If uninstall not called from WordPress, then exit.
-if (!defined( 'WP_UNINSTALL_PLUGIN')) {
-	exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
 
-function drop_tables() {
-	global $wpdb;
-	$sql = "DROP TABLE 'wp__vpos_woocommerce_transactions_1_1'";
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+function drop_tables()
+{
+    global $wpdb;
+    $sql = "DROP TABLE 'wp__vpos_woocommerce_transactions_1_1'";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
 }
 
