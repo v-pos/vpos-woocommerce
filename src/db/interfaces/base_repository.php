9e1328a72e38c505366940159629c501b0e0769c
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-11 09:08:24 +0100

    Add interface

---
+++ src/db/interfaces/base_repository.php
@@ -0,0 +1,13 @@
+<?php
+
+interface BaseRepository
+{
+    public function create_table();
+    public function get($id);
+    // To upgrade table see wordpress documentation: https://codex.wordpress.org/Creating_Tables_with_Plugins
+    public function update($id, $model);
+    public function delete($id);
+    public function insert($model);
+}
+
+?>
